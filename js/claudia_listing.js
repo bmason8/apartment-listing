90f5d128a371fa0b684bb537f157010d4026548f
Claudia Cuevas <omordn@gmail.com>
2015-01-27 12:58:23 -0800

    partial working checkbox filter

--- js/claudia_listing.js
+++ js/claudia_listing.js
@@ -1,77 +1,48 @@
 $(function() {
 
-  function Property(name, bed, price, cats, dogs, location) {
-    this.name = name;
-    this.bed = bed;
-    this.price = price;
-    this.cats = cats;
-    this.dogs = dogs;
-    this.location = location;
-  }
-
-  Property.prototype.toHtml = function() {
-    return "<li class='property' data-location='" + this.location + "' data-price='" + this.price + "'>" + "I work " + this.name + " " + this.bed + "</li>";
-  }
-
-  function Seattle() {
-    thisSeattle = this;
-    this.list = [
-      {"name" : "The Manor", "bed" : "studio", "price" : 900, "cats" : "no", "dogs" : "no", "location" : "Seattle"},
-      {"name" : "The Manor", "bed" : "1 bed", "price" : 1100, "cats" : "yes", "dogs" : "no", "location" : "Seattle"}
-    ];
-
-    $.each(thisSeattle.list, function() {
-      var addProperty = new Property(this.name, this.bed, this.price, this.cats, this.dogs, this.location);
-      $("#listings").append(addProperty.toHtml);
-    });
-  }
-
-  function Renton() {
-    thisRenton = this;
-    this.list = [
-      {"name" : "The Manor", "bed" : "studio", "price" : 900, "cats" : "no", "dogs" : "no", "location" : "renton"},
-      {"name" : "The Manor", "bed" : "1 bed", "price" : 1100, "cats" : "yes", "dogs" : "no", "location" : "renton"}
-    ];
-
-    $.each(thisRenton.list, function() {
-      var addProperty = new Property(this.name, this.bed, this.price, this.cats, this.dogs, this.location);
-      $("#listings").append(addProperty.toHtml);
-    });
-  }
-  // function Search() {
-  // 	switch ($("#locationSelect").val()) {
-  // 		case "seattle":
-  // 			$(".property").each(function() {
-  // 				if($(this).data("location") !== "seattle") {
-  //  					$(this).fadeOut(1000);		
-  // 				}
-  // 			});
-  // 		break;
-  // 		case "renton":
-  // 			$(".property").each(function() {
-  // 				if ($(this).data("location") != "renton") {
-  // 					$(this).fadeOut(1000);
-  // 				}
-  // 			});	
-  //  		break
-  // 		case "capitolHill":
-  // 			$(".property").each(function() {
-  // 				if ($(this).data("location") != "capitolHill") {
-  // 					$(this).fadeOut(1000);
-  // 				}
-  // 			});	
-  // 		break;
-  // 		default:
-  // 			alert("Nothing is Selected");
-  // 	}
+  // var seattle = function() {
+  //   switch ($('.property').data('pets')) {
+  //     case "true":
+  //       $('.property').each
+  //   }
   // }
+  // is(":checked") && $("input").data("pets") == "true")
+  
+  function Search() {
+  	switch ($("#locationSelect").val()) {
+  		case "seattle":
+  			$(".property").each(function() {
+  				if (($(this).data("location") != "seattle")) {
+            if ($("input:checkbox").is(":checked")) {
+                // $(".property").data("pets").fadeIn(1000);
+                $(".property").data("pets", "false").fadeOut(1000);
+            } else {
+              $(this).fadeOut(1000);
+            }
+          }
+  			});
+  		break;
+  		case "renton":
+  			$(".property").each(function() {
+  				if ($(this).data("location") != "renton") {
+  					$(this).fadeOut(1000);
+  				}
+  			});	
+   		break
+  		case "capitolHill":
+  			$(".property").each(function() {
+  				if ($(this).data("location") != "capitolHill") {
+  					$(this).fadeOut(1000);
+  				}
+  			});	
+  		break;
+  		default:
+  			alert("Nothing is Selected");
+  	}
+  }
 
-  property = new Property();
-  $("#list").on("click", function() {
-    // alert("I work")
-    if ($('#locationSelect').val() == "Seattle") {
-      Seattle();
-    }  
-  });
+  
+  $("#list").on("click", Search);
+
 
 });
